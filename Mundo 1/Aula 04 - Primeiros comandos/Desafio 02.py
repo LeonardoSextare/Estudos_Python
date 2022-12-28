2b54e1fe26ea11d2beba7ee047ab17faac9e843c
Leonardo Sextare <leonardosextare@gmail.com>
2022-12-28 15:32:50 -0300

    Teste

---
+++ Mundo 1/Aula 04 - Primeiros comandos/Desafio 02.py
@@ -0,0 +1,5 @@
+dia = input('Qual dia você nasceu?')
+mes = input('Qual mês você nasceu?')
+ano = input('Qual ano você nasceu?')
+
+print('Você nasceu dia', dia, 'de', mes, 'do ano de', ano)
---
+++ Mundo 1/Aula 04 - Primeiros comandos/Desafio 03.py
@@ -0,0 +1,6 @@
+print('Calculadora de Soma')
+num1 = input('Digite um numero: ')
+num2 = input('Digite outro numero: ')
+
+print(num1 + num2)
+print(int(num1) + int(num2))
