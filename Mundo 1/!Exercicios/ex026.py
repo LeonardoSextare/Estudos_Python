beda848ec9090791fbf37e67e68ed8cfa76fd001
Leonardo Sextare <leonardosextare@gmail.com>
2022-12-30 23:40:33 -0300

    Aula 09 Exercicios
    Finalizados

--- Mundo 1/!Exercicios/ex026.py
+++ Mundo 1/!Exercicios/ex026.py
@@ -1,1 +1,7 @@
 #Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+frase = str(input('Digite uma frase: \n')).strip().lower()
+
+print(f"""A letra "A" foi digitada {frase.count('a')} vezes
+Apareceu pela primeira vez na posição: {frase.find('a')+1}
+Apareceu pela ultima vez na posição: {frase.rfind('a')+1}""")
--- Mundo 1/!Exercicios/ex027.py
+++ Mundo 1/!Exercicios/ex027.py
@@ -1,1 +1,5 @@
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nome = str(input('Digite seu nome completo: \n')).strip().title()
+
+print(f'Seu primeiro nome é: {nome[:nome.find(" ")]}\nSeu ultimo nome é: {nome[nome.rfind(" ")+1:]}')
